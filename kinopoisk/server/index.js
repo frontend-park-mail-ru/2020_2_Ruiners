a756697986922338d4b16b22386b5c463648cd6b
ERIK NABIEV <er2000nabiev@mail.ru>
2020-09-28 16:52:27 +0300

    server done

---
+++ kinopoisk/server/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const path = `./public${req.url === '/' ? '/index.html': req.url}`;
+    fs.readFile(path, (err, file) => {
+        if (err) {
+            res.write('error');
+            res.end();
+            return;
+        }
+        console.log(path, err)
+        res.write(file);
+        res.end();
+    });
+});
+
+server.listen(3000);
