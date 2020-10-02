f4584a646ab4a0a12e7af28596b153865c1c3e19
govyadkin <mgovyadkin@gmail.com>
2020-10-02 16:02:11 +0300

    profileSPA

--- kinopoisk/public/main.js
+++ kinopoisk/public/main.js
@@ -1,4 +1,3 @@
-
 const application = document.getElementById('app');
 const nav = document.getElementById('navbar');
 
@@ -16,7 +15,7 @@ const menu = {
         text: 'Войти',
     },
     profile: {
-        href: '/static/profile.html',
+        href: '/profile',
         text: 'Профиль',
     },
 };
@@ -48,7 +47,7 @@ function createNavbar() {
 
 function menuPage() {
     application.innerHTML = '';
-    application.className = ''
+    application.className = '';
     Object.keys(menu).map((menuKey) => {
         const {href, text} = menu[menuKey];
         const menuItem = document.createElement('a');
@@ -60,7 +59,7 @@ function menuPage() {
 }
 
 createNavbar();
-menuPage()
+menuPage();
 
 
 function createLi(className, child) {
@@ -77,6 +76,14 @@ function createA(href, text) {
     return a;
 }
 
+function createDiv(cla, child) {
+    const div = document.createElement('div');
+    div.className = cla;
+
+    child.appendChild(div);
+    return div;
+}
+
 function createInput(type, text) {
     const input = document.createElement('input');
     input.type = type;
@@ -86,6 +93,7 @@ function createInput(type, text) {
 
 const signupLink = application.querySelector('[data-section="signup"]');
 const loginLink = application.querySelector('[data-section="login"]');
+const profileLink = application.querySelector('[data-section="profile"]');
 
 signupLink.addEventListener('click', (event) => {
     event.preventDefault();
@@ -95,24 +103,24 @@ signupLink.addEventListener('click', (event) => {
     const form = document.createElement('form');
     form.className = 'wrapper__form register';
     body.appendChild(form);
-    const header = document.createElement('h2')
-    header.textContent = 'Регистрация'
-    header.style = 'color:#FFFFFF; margin-left: 10px'
-    form.appendChild(header)
-    const loginInput = createInput('login', 'Логин')
-    const emailInput = createInput('email', 'e-mail')
-    const passwordInput = createInput('password', 'Пароль')
-    form.appendChild(loginInput)
-    form.appendChild(emailInput)
-    form.appendChild(passwordInput)
-    const button = document.createElement('button')
-    button.href = '/'
-    button.textContent = 'Регистрация'
-    button.className = 'secondary'
-    form.appendChild(button)
-    const linkLogin = createA('/login', 'Войти в имеющийся')
-    linkLogin.style = 'color: #FFFFFF; margin-left: 10px'
-    form.appendChild(linkLogin)
+    const header = document.createElement('h2');
+    header.textContent = 'Регистрация';
+    header.style = 'color:#FFFFFF; margin-left: 10px';
+    form.appendChild(header);
+    const loginInput = createInput('login', 'Логин');
+    const emailInput = createInput('email', 'e-mail');
+    const passwordInput = createInput('password', 'Пароль');
+    form.appendChild(loginInput);
+    form.appendChild(emailInput);
+    form.appendChild(passwordInput);
+    const button = document.createElement('button');
+    button.href = '/';
+    button.textContent = 'Регистрация';
+    button.className = 'secondary';
+    form.appendChild(button);
+    const linkLogin = createA('/login', 'Войти в имеющийся');
+    linkLogin.style = 'color: #FFFFFF; margin-left: 10px';
+    form.appendChild(linkLogin);
 });
 
 loginLink.addEventListener('click', (event) => {
@@ -143,4 +151,95 @@ loginLink.addEventListener('click', (event) => {
         event.preventDefault();
         menuPage();
     });
+});
+
+profileLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    application.innerHTML = '';
+
+    const body = document.getElementById('body');
+
+    const div = createDiv('page', body);
+    const divshadow = createDiv('shadow', div);
+
+    const ul = document.createElement('ul');
+    ul.className = 'top-menu';
+    divshadow.appendChild(ul);
+
+    const menuItem = document.createElement('li');
+    const menuItema = document.createElement('span');
+
+    menuItema.textContent = "Профиль";
+
+    menuItem.appendChild(menuItema);
+    ul.appendChild(menuItem);
+
+    const menu_top = {
+        rech: {
+            href: '/rech',
+            text: 'Рецензии',
+        },
+        mark: {
+            href: '/mark',
+            text: 'Оценки',
+        },
+        films: {
+            href: '/films',
+            text: 'Фильмы',
+        },
+        stars: {
+            href: '/stars',
+            text: 'Звёзды',
+        },
+    };
+
+    Object.keys(menu_top).map((menuKey) => {
+        const {href, text} = menu_top[menuKey];
+        const menuItem = document.createElement('li');
+        const menuItema = createA(href, text);
+        menuItema.dataset.section = menuKey;
+        menuItem.appendChild(menuItema);
+        ul.appendChild(menuItem);
+    });
+
+    const divLeft = createDiv('profileInfoWrapLeft', divshadow);
+    const divAvatar = createDiv('avatarUserBoxP', divLeft);
+
+    const img = document.createElement('img');
+    img.src = 'static/images/user-no-big.gif';
+
+    divAvatar.appendChild(img);
+
+    const button = document.createElement('button');
+    button.className = 'secondary';
+    button.textContent = 'Изменить данные';
+    button.href = '/hh';
+
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        application.innerHTML = '';
+        menuPage();
+    });
+
+    divLeft.appendChild(button);
+
+
+    const divRight = createDiv('profileInfoWrapRight', divshadow);
+    const divInfo = createDiv('infoUser', divRight);
+
+    const nick = document.createElement('a');
+    nick.className = 'nick_name';
+    nick.textContent = 'mgovyadkinya';
+
+    divInfo.appendChild(nick);
+
+    const divInfoAuth = createDiv('infoUserAuth', divRight);
+
+    const span1 = document.createElement('span');
+    span1.textContent = 'Регистрация: 11 марта 2020';
+    divInfoAuth.appendChild(span1);
+
+    const span2 = document.createElement('span');
+    span2.textContent = 'Рейтинг комментариев:';
+    divInfoAuth.appendChild(span2);
 });
