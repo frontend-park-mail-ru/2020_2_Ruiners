cb92a9826cb9bb154bd0618c148bea780a49aa4d
govyadkin <mgovyadkin@gmail.com>
2020-10-06 11:38:18 +0300

    validation

--- kinopoisk/server/index.js
+++ kinopoisk/server/index.js
@@ -1,9 +1,8 @@
-'use strict';
-
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const { XMLHttpRequest } = require('xmlhttprequest');
 const childProc = require('child_process');
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const body = require('body-parser');
 
@@ -12,31 +11,30 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 const port = process.env.PORT || 3000;
 
-app.get('*', function (req, res) {
-  var xhr = new XMLHttpRequest();
-  var domain = req.headers.host
-  domain = 'http://' + domain.substring(0, domain.length - 5) + ':8080'
-  var url = domain + req.url;
-  xhr.open("GET", url, false);
-  xhr.setRequestHeader("Content-type", "text/html");
+app.get('*', (req, res) => {
+  const xhr = new XMLHttpRequest();
+  let domain = req.headers.host;
+  domain = `http://${domain.substring(0, domain.length - 5)}:8080`;
+  const url = domain + req.url;
+  xhr.open('GET', url, false);
+  xhr.setRequestHeader('Content-type', 'text/html');
   xhr.crossOrigin = true;
   xhr.withCredentials = true;
   xhr.send(null);
   res.status(xhr.status).json(xhr.responseText);
 });
 
-app.post('*', function (req, res) {
-  var xhr = new XMLHttpRequest();
-  var domain = req.headers.host
-  domain = 'http://' + domain.substring(0, domain.length - 5) + ':8080'
-  var url = domain + req.url;
-  xhr.open("POST", url, false);
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+app.post('*', (req, res) => {
+  const xhr = new XMLHttpRequest();
+  let domain = req.headers.host;
+  domain = `http://${domain.substring(0, domain.length - 5)}:8080`;
+  const url = domain + req.url;
+  xhr.open('POST', url, false);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
   xhr.crossOrigin = true;
   xhr.withCredentials = true;
   xhr.send(JSON.stringify(req.body));
   res.status(xhr.status).json(xhr.responseText);
 });
 
-
-app.listen(port)
+app.listen(port);
