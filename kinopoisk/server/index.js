58b3d3960fd4edbe9841de7cd62903d193ced712
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-10-06 08:58:09 +0300

    test

--- kinopoisk/server/index.js
+++ kinopoisk/server/index.js
@@ -1,18 +1,42 @@
-const fs = require('fs');
-const http = require('http');
+'use strict';
 
-const server = http.createServer((req, res) => {
-  const path = `./public${req.url === '/' ? '/index.html' : req.url}`;
-  fs.readFile(path, (err, file) => {
-    if (err) {
-      res.write('error');
-      res.end();
-      return;
-    }
-    console.log(path, err);
-    res.write(file);
-    res.end();
-  });
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const childProc = require('child_process');
+const express = require('express');
+const path = require('path');
+const app = express();
+const body = require('body-parser');
+
+app.use(body.json());
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+const port = process.env.PORT || 3000;
+
+app.get('*', function (req, res) {
+  var xhr = new XMLHttpRequest();
+  var domain = req.headers.host
+  domain = 'http://' + domain.substring(0, domain.length - 5) + ':8080'
+  var url = domain + req.url;
+  xhr.open("GET", url, false);
+  xhr.setRequestHeader("Content-type", "text/html");
+  xhr.crossOrigin = true;
+  xhr.withCredentials = true;
+  xhr.send(null);
+  res.status(xhr.status).json(xhr.responseText);
 });
 
-server.listen(3000);
+app.post('*', function (req, res) {
+  var xhr = new XMLHttpRequest();
+  var domain = req.headers.host
+  domain = 'http://' + domain.substring(0, domain.length - 5) + ':8080'
+  var url = domain + req.url;
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.crossOrigin = true;
+  xhr.withCredentials = true;
+  xhr.send(JSON.stringify(req.body));
+  res.status(xhr.status).json(xhr.responseText);
+});
+
+
+app.listen(port)
