47c0173b0e969cb1456768f8160e9dedc7d3b696
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-10-06 12:30:39 +0300

    ajaxM3

--- kinopoisk/server/index.js
+++ kinopoisk/server/index.js
@@ -12,4 +12,25 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 const port = process.env.PORT || 3000;
 
 
+app.post('*', function (req, res) {
+  var xhr = new XMLHttpRequest();
+  var domain = req.headers.host
+  domain = 'http://' + domain.substring(0, domain.length - 5) + ':8000'
+  var url = domain + req.url;
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+app.post('*', (req, res) => {
+  const xhr = new XMLHttpRequest();
+  let domain = req.headers.host;
+  domain = `http://${domain.substring(0, domain.length - 5)}:8080`;
+  const url = domain + req.url;
+  xhr.open('POST', url, false);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.crossOrigin = true;
+  xhr.withCredentials = true;
+  xhr.send(JSON.stringify(req.body));
+  res.status(xhr.status).json(xhr.responseText);
+});
+
 app.listen(port);
