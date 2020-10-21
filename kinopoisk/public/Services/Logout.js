5a63d00b69276b5d804e3e225cdab05094d1f2e1
Chekryzhov Arkadiy <chekryzhov2000@mail.ru>
2020-10-21 23:44:31 +0300

    new ajax

--- kinopoisk/public/Services/Logout.js
+++
@@ -1,8 +0,0 @@
-export default class Logout {
-    static logout(callback) {
-        ajaxGetUsingFetch({ url: '/logout', body: null })
-            .then((res) => {
-                callback(res);
-            });
-    }
-}
