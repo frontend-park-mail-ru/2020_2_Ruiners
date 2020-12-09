db30d7573b228b43c79cb0f93f772cf7e0028b0c
ErikNabiev <er2000nabiev@mail.ru>
2020-12-10 01:06:49 +0300

    domain

--- kinopoisk/public/config.js
+++ kinopoisk/public/config.js
@@ -1,4 +1,4 @@
 export const application = document.getElementById('app');
 export const nav = document.getElementById('navbar');
 
-export const domain = 'http://kino-park.online:8000'; // kino-park.online
+export const domain = 'http://95.163.208.72:8000'; // kino-park.online
