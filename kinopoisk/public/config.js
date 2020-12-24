40e67884b718f4c089aad8936e821d28d1c93741
ErikNabiev <er2000nabiev@mail.ru>
2020-12-24 13:13:37 +0300

    config

--- kinopoisk/public/config.js
+++ kinopoisk/public/config.js
@@ -1,4 +1,4 @@
 export const application = document.getElementById('app');
 export const nav = document.getElementById('navbar');
 
-export const domain = 'http://localhost:8000'; // https://kino-park.online
+export const domain = 'https://kino-park.online'; // https://kino-park.online
